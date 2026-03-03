20404ce7e2667691064ae6006c1cc6eb251806a3
laurenyip <laurenyip10@gmail.com>
2026-03-02 23:07:19 -0800

    stp

--- src/components/Sessions.tsx
+++ src/components/Sessions.tsx
@@ -13,30 +13,27 @@ export default function Sessions() {
   };
 
   // Sessions with specific dates, links and locations
-  // February 6, 2026 is a Friday
-  // February 9, 2026 is a Monday
-  // February 20, 2026 is a Friday
-  // February 23, 2026 is a Monday
+  // March 6, 13, 20, 27 2026 – all Fridays at East tower, Burnaby
   const sessions = [
     {
-      date: new Date(2026, 1, 6), // Friday, February 6, 2026
-      location: 'East Tower, SFU Burnaby',
-      link: 'https://luma.com/e6fxwjia'
+      date: new Date(2026, 2, 6), // Friday, March 6, 2026
+      location: 'East tower, Burnaby',
+      link: 'https://luma.com/cqnppunl'
     },
     {
-      date: new Date(2026, 1, 9), // Monday, February 9, 2026
-      location: 'SRYE 5050, SFU Surrey',
-      link: 'https://luma.com/lexxeyy4'
+      date: new Date(2026, 2, 13), // Friday, March 13, 2026
+      location: 'East tower, Burnaby',
+      link: 'https://luma.com/pl8m1x2q'
     },
     {
-      date: new Date(2026, 1, 20), // Friday, February 20, 2026
-      location: 'East Tower, SFU Burnaby',
-      link: 'https://luma.com/meo2rle8'
+      date: new Date(2026, 2, 20), // Friday, March 20, 2026
+      location: 'East tower, Burnaby',
+      link: 'https://luma.com/20w4c7s4'
     },
     {
-      date: new Date(2026, 1, 23), // Monday, February 23, 2026
-      location: 'SRYE 5050, SFU Surrey',
-      link: 'https://luma.com/mdys22f6'
+      date: new Date(2026, 2, 27), // Friday, March 27, 2026
+      location: 'East tower, Burnaby',
+      link: 'https://luma.com/h8sn08u9'
     }
   ];
 
